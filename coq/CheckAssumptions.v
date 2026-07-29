5a39bd4e423876744060576ceca4d9d2c0321923
Yaoharee Lahtee <127247155+morrocwi@users.noreply.github.com>
2026-07-29 13:15:22 +0700

    Add explicit assumption checks

---
+++ coq/CheckAssumptions.v
@@ -0,0 +1,6 @@
+Require Import IDM_KeystoneKernel.
+Print Assumptions keystone_zero_iff_edge.
+Print Assumptions keystone_zero_iff_component.
+Print Assumptions kernel_add.
+Print Assumptions kernel_scale.
+Print Assumptions indist_trans.
